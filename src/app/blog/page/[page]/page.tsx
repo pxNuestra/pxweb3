d23bd9f5d0d369e8784743eba95ae1e2358526c6
Muhammad F. Nuestra <93759378+pxNuestra@users.noreply.github.com>
2025-11-05 13:19:55 +0700

    test

--- src/app/blog/page/[page]/page.tsx
+++ src/app/blog/page/[page]/page.tsx
@@ -1,36 +1,24 @@
 import ListLayout from '../../ListLayout'
 import { allCoreContent, sortPosts } from '@/lib/utils'
-import { allBlogs } from 'contentlayer/generated'
 
 const POSTS_PER_PAGE = 5
 
 export const generateStaticParams = async () => {
-  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
-  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
-
-  return paths
+  // Tidak ada post, jadi totalPages = 0
+  return []
 }
 
 export default function Page({ params }: { params: { page: string } }) {
-  const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = parseInt(params.page as string)
-  const initialDisplayPosts = posts.slice(
-    POSTS_PER_PAGE * (pageNumber - 1),
-    POSTS_PER_PAGE * pageNumber
-  )
+  const posts: any[] = []  // Kosongkan posts
+  const initialDisplayPosts: any[] = []  // Kosongkan tampilan awal
   const pagination = {
-    currentPage: pageNumber,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+    currentPage: 1,
+    totalPages: 0,
   }
 
   return (
-    <>
-      <ListLayout
-        posts={posts}
-        initialDisplayPosts={initialDisplayPosts}
-        pagination={pagination}
-        title="All Posts"
-      />
-    </>
+    <ListLayout
+      title="All Posts"
+    />
   )
 }
